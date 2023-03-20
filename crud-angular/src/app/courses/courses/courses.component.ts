16dfe5833f569f798a09d4258c44d61dd51f5212
CB MAICON GILSON BARBOSA <21107331401@pm.df.gov.br>
2023-03-20 08:59:25 -0300

    Crud Angular Spring

---
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -0,0 +1,48 @@
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ErrorDialogComponent } from './../../shared/components/error-dialog/error-dialog.component';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Course } from './../model/course';
+import { CoursesService } from './../services/courses.service';
+
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss']
+})
+export class CoursesComponent {
+
+  courses$: Observable<Course[]>;
+
+  displayedColumns = ['name', 'category', 'actions'];
+
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute) {
+
+
+    this.courses$ = this.coursesService.list()
+      .pipe(
+        catchError(error => {
+          this.onError('Erro ao carregar');
+          return of([])
+        })
+      );
+  }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+}
